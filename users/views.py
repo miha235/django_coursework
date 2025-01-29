c26d1f8a063002b40dffedb15ec94d034bdc0782
Victoria <Victoria23miha@gmail.com>
2025-01-30 00:16:36 +0300

    Исправление ошибок

--- users/views.py
+++ users/views.py
@@ -1,43 +1,59 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.sites.shortcuts import get_current_site
-from django.urls import reverse_lazy,reverse
+from django.urls import reverse_lazy, reverse
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from django.contrib.auth.tokens import default_token_generator as token_generator
 from django.core.mail import send_mail
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
-from mailing.forms import CustomUserCreationForm,CustomAuthenticationForm,CustomUser
-
+from mailing.forms import CustomUserCreationForm, CustomAuthenticationForm, CustomUser
 
 class SignUpView(CreateView):
-    ''' Контроллер для создания нового Пользователя с отправкой email подтверждения '''
+    ''' Контроллер для регистрации нового пользователя '''
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/registration/signup.html'
 
     def form_valid(self, form):
-        # Сохранение пользователя, но ещё не активированного
+        # Создаём пользователя, но пока не активируем
         user = form.save(commit=False)
-        user.is_active = False  # Деактивируем пользователя до подтверждения
+        user.is_active = False
         user.save()
 
-        # Генерация токена для подтверждения email
+        # Генерация токена и ID пользователя
         token = token_generator.make_token(user)
-        uid = urlsafe_base64_encode(str(user.pk).encode('utf-8')).decode()
+        uid = urlsafe_base64_encode(str(user.pk).encode('utf-8'))
 
-        # Отправка email для подтверждения
+        # Домен текущего сайта
         current_site = get_current_site(self.request)
-        subject = 'Подтвердите вашу почту'
-        message = render_to_string('users/activation_email.html', {
+
+        # Формируем email-сообщение
+        subject = 'Подтверждение профиля'
+        message = render_to_string('users/registration/activation_email.html', {
             'user': user,
+            'username': user.username,
             'domain': current_site.domain,
             'uid': uid,
             'token': token,
         })
-        send_mail(subject, message, 'from@example.com', [user.email])
-        return render(self.request, 'users/registration/confirmation_sent.html')
+
+        # Извлекаем ссылку из HTML-сообщения (можно сделать это вручную или оставить как есть)
+        activation_link = f"http://{current_site.domain}/users/verify/{urlsafe_base64_encode ( str ( user.pk ).encode ( 'utf-8' ) )}/{token}/"
+        message_plain = f"Здравствуйте, {user.username}!\n\n" \
+                        f"Спасибо за регистрацию на нашем сайте. Чтобы активировать свой профиль, перейдите по следующей ссылке:\n\n" \
+                        f"{activation_link}\n\n" \
+                        "Если вы не регистрировались на нашем сайте, просто проигнорируйте это письмо."
+
+        # Отправка email
+        send_mail(
+            subject, message_plain, 'v_23_d_1989@mail.ru', [user.email], fail_silently=False
+        )
+
+        # Возвращаем страницу подтверждения
+        return render(self.request, 'users/registration/conformation_sent.html')
+
 
 # Контроллер для подтверждения email
 def verify_email(request, uidb64, token):
@@ -50,28 +66,20 @@ def verify_email(request, uidb64, token):
 
     if user is not None and token_generator.check_token(user, token):
         user.is_active = True
-        user.is_verified = True
+        user.is_verified = True  # Если в модели есть это поле
         user.save()
         return redirect('login')
     else:
-        return render(request, 'users/activation_invalid.html')
-'''
-def verify_email(request, user_id):
-    #Функция проверки верификации пользователя
-    user = CustomUser.objects.get(id=user_id)
-    user.is_verified = True
-    user.save()
-    return redirect('login')
-'''
-class CustomLogoutView(LogoutView):
-    success_url = reverse_lazy('main_page')
+        return render(request, 'users/registration/activation_invalid.html')
 
-    def get_success_url(self):
-        """Переопределите метод, чтобы перенаправить пользователя после успешного входа."""
-        return self.success_url
+
+class CustomLogoutView(LogoutView):
+    ''' Контроллер для выхода пользователя '''
+    next_page = reverse_lazy('main_page')
+
 
 class CustomLoginView(LoginView):
-    ''' Контроллер для логина Пользователя'''
+    ''' Контроллер для входа пользователя '''
     form_class = CustomAuthenticationForm
     template_name = 'users/registration/login.html'
 
@@ -83,4 +91,3 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return self.get_redirect_url() or reverse('client_list')
-
